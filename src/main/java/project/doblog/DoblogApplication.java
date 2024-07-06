f991b3388d403678e997739933957334435621db
kimdohyung <pdohyung@gmail.com>
2024-07-06 15:37:46 +0900

    initial commit

---
+++ src/main/java/project/doblog/DoblogApplication.java
@@ -0,0 +1,13 @@
+package project.doblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/doblog/DoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package project.doblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
